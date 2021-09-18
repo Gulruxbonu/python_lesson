6aeaf4c22865fedbd4d5b76f23ac08ecb411513b
Gulruxbonu <90915714+Gulruxbonu@users.noreply.github.com>
2021-09-18 15:43:10 +0500

    yangi fayllar qoshildi
    to'qqizinchi,o'ninchi darslar qo'shildi

---
+++ o'ninchi-dars.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 15 17:04:35 2021
+
+@author: WWW
+"""
+#10-DARS IF ELSE HAQIDA TUSHUNCHA  TARMOQLASH
+# IF -agar degani 
+
+#avtolar = ['audi','bmw','volvo','kia','hyundai']
+#print(avtolar)
+
+#for avto in avtolar:  #avtolar ichidagi har bir avto uchun...
+#   if avto == 'bmw':  #agar avto bmwga teng bols
+#       print(avto.upper())  #avto nomini hamma harflari katta bolsin
+ #  else:  #aks holda...
+#      print(avto.title())  #avto nomini faqat 1-harfi katta bolsin
+       
+#ism = 'Ali'
+
+#ism.lower() == 'ali'
+#YUQORIDA BIZ TENGMI SHARTINI KO'RDIK,ENDI TENG EMAS SHARTINI KORAMIZ
+
+#ism = input('Ismingiz nima ?\n>>>')  #foydalanuvchi ismini sorash
+#if ism.lower() != 'ali':  #agar ism aliga teng bolmasa
+#   print(f"Uzr,{ism.title()} biz Alini kutyapmiz.")
+#else:
+#  print("Salom, Ali")
+
+
+#javob = float(input("12*6 nechiga teng?>>>"))
+#if javob != 72:
+#  print("Javob xato!")
+
+
+#yosh = int(input("Yoshingiz nechchida?>>>"))
+#if yosh >= 18: #yosh 18dan katta yoki teng bolsa
+#  print("Xush kelibsiz")
+#else:  #aks xolda
+#  print('Kirish mumkin emas')
+  
+   
+   
+#Login = input("Yangi login tanlang:")
+#if len(Login)<5:   #login uzunligini tekshiramiz
+#  print("Login 5 harfdan ko'proq bo'lishi shart!")
+  
+  
+#yil = int(input("Tug'ilgan yilingizni kiriting:"))
+#if 2020-yil < 18:    #foydalanuvchining yoshini hisoblaymiz
+#    print(f"Yoshingiz {2020-yil}da ekan.")
+#   print("Kirish mumkin emas!")
+#else:
+#    print("Xush kelibsiz!")
+    
+    
+#yosh = int(input("Yoshingiz nechida?>>>"))
+#if yosh>65: print("Siz COVID-19 risk guruhida ekansiz")
+
+#x,y, = 25, 50 #x=25 y=50
+#print("x,y") if x>y else print("x<y")
+ 
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+      
+       
+       
---
+++ python9-dars.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 14 16:35:48 2021
+
+@author: WWW
+"""
+
+# 9 -DARS FOR TSKILI (TAKRORLASH OPERATORI) BILAN TANISHAMIZ
+
+#for sikli har bir mehmon uchun alohida salom deb yozib o'tirmay 
+#bittada hamma mehmonlar uchun alohida alohida salom deb yozish imkonini beradi
+# M-N quyidagi misol
+
+#mehmonlar = ['Ali', 'Vali', 'Hasan', 'Olim']
+#for mehmon in mehmonlar:
+#    print("Salom", mehmon)
+#    print("Hayr,", mehmon)  # bu yerda printni surib oldinroqqa yozish juda muhim
+    
+    
+# yana bir misol
+#mehmonlar = ['Ali', 'Vali', 'Hasan', 'Olim']
+#for mehmon in mehmonlar:
+#    print(f"Hurmatli {mehmon}, sizni 20-avgust kuni nahor oshga taklif qilamiz") 
+#    print("Hurmat bilan Hojahmedovlar oilasi")
+    
+# for siklida sonlar bn ishlash
+#sonlar = list(range(1,11))
+#for son in sonlar:
+#   print(f"{son} ning kvadrati {son**2} ga teng")
+
+#yana bir misol sonlarga
+#sonlar = list(range(11)) #1 dan 10gacha sonlar royxatini yozish
+#sonlar_kvadrati = []  #bo'sh royxat yaratamiz
+#for son in sonlar:   # sonlar dagi har bir son uchun
+#      sonlar_kvadrati.append(son**2)  # uning kvadratini hisoblab chiqarib beradi
+      
+#print(sonlar)
+#print(sonlar_kvadrati)
+
+# MISOL.....
+#dostlar = []  #bosh royhat
+#print("5 ta eng yaqin dostingiz kim?")
+#for n in range(5):   # n bu yerda 0 dan 4 gacha qiymatlarni bildiradi
+#   dostlar.append(input(f"{n+1}-dostingizni ismini kiriting:"))
+#print(dostlar)
+      
+    
+    
